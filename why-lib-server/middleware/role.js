0e57a00eec85cc00f8a420cc1b0fc697863b49b2
lvdat <lvdat13@gmail.com>
2025-03-28 06:17:36 +0700

    middleware: role

---
+++ why-lib-server/middleware/role.js
@@ -0,0 +1,12 @@
+// Middleware kiểm tra role
+exports.checkRole = (roles) => {
+    return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+        return res.status(403).json({ 
+          success: false, 
+          message: 'Bạn không có quyền truy cập tài nguyên này' 
+        });
+      }
+      next();
+    };
+  };
