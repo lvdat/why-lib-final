672282e0f90b1bdac89ffac458dcae54d8893b85
lvdat <lvdat13@gmail.com>
2025-03-28 06:13:53 +0700

    model: Sach

---
+++ why-lib-server/models/Sach.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const SachSchema = new mongoose.Schema({
+  MASACH: { type: String, required: true, unique: true },
+  TENSACH: { type: String, required: true },
+  DONGIA: { type: Number, required: true },
+  SOQUYEN: { type: Number, required: true, min: 0 },
+  NAMXUATBAN: { type: Number, required: true },
+  MANXB: { type: mongoose.Schema.Types.ObjectId, ref: 'NhaXuatBan', required: true },
+  TACGIA: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Sach', SachSchema);
